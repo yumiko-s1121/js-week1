d8b2e324816abeed76166fda8cf5d1041684c52e
yumiko-s <saitoh.fw@gmail.com>
2025-12-26 13:59:09 +0900

    Day2: input and if in JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-function sayHello() {
-    alert("Hello JavaScript!")
+// ---- 12/25 作成部分 ----
+// function sayHello() {
+//    alert("Hello JavaScript!")
+//}
+
+// ---- 12/26 作成部分 ----
+function sayHello(){
+    let age = document.getElementById("age").value;
+    age = Number(age);
+
+    if (age >= 20) {
+        alert("You are an adult")
+    } else {
+        alert("You are a child")
+    }
 }
